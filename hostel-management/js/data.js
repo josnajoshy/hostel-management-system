c5af26b4cd9d4e83b3a1e99efdf9aa3d0fb77c09
gitkevin06 <2006kevinjames@gmail.com>
2026-03-10 20:53:13 +0530

    Add files via upload

---
+++ hostel-management/js/data.js
@@ -0,0 +1,7 @@
+const students = [
+    { id: 101, name: "Rahul", department: "CSE", room: 204 },
+    { id: 102, name: "Arjun", department: "ECE", room: 105 },
+    { id: 103, name: "Vikram", department: "ME", room: 210 },
+    { id: 104, name: "Sanjay", department: "CSE", room: 111 },
+    { id: 105, name: "Kiran", department: "EEE", room: 305 }
+];
